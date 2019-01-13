dd0b01ffef0e7462dd8ff085a3c65e0a38ff746e
Liu Yingduo <15000275737@163.com>
2019-01-13 22:52:49 +0800

    first commit

---
+++ code/Xception.py
@@ -0,0 +1,233 @@
+
+import os
+import glob
+import math
+import pandas as pd
+import torch 
+import random
+from torch import nn, optim
+from torch.autograd import Variable
+import torch.nn.functional as F
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class SeparableConv2d(nn.Module):
+    def __init__(self,in_channels,out_channels,kernel_size=1,stride=1,padding=0,dilation=1,bias=False):
+        super(SeparableConv2d,self).__init__()
+
+        self.conv1 = nn.Conv2d(in_channels,in_channels,kernel_size,stride,padding,dilation,groups=in_channels,bias=bias)
+        self.pointwise = nn.Conv2d(in_channels,out_channels,1,1,0,1,1,bias=bias)
+    
+    def forward(self,x):
+        x = self.conv1(x)
+        x = self.pointwise(x)
+        return x
+
+
+class Block(nn.Module):
+    def __init__(self,in_filters,out_filters,reps,strides=1,start_with_relu=True,grow_first=True):
+        super(Block, self).__init__()
+
+        if out_filters != in_filters or strides!=1:
+            self.skip = nn.Conv2d(in_filters,out_filters,1,stride=strides, bias=False)
+            self.skipbn = nn.BatchNorm2d(out_filters)
+        else:
+            self.skip=None
+        
+        self.relu = nn.ReLU(inplace=True)
+        rep=[]
+
+        filters=in_filters
+        if grow_first:
+            rep.append(self.relu)
+            rep.append(SeparableConv2d(in_filters,out_filters,3,stride=1,padding=1,bias=False))
+            rep.append(nn.BatchNorm2d(out_filters))
+            filters = out_filters
+
+        for i in range(reps-1):
+            rep.append(self.relu)
+            rep.append(SeparableConv2d(filters,filters,3,stride=1,padding=1,bias=False))
+            rep.append(nn.BatchNorm2d(filters))
+        
+        if not grow_first:
+            rep.append(self.relu)
+            rep.append(SeparableConv2d(in_filters,out_filters,3,stride=1,padding=1,bias=False))
+            rep.append(nn.BatchNorm2d(out_filters))
+
+        if not start_with_relu:
+            rep = rep[1:]
+        else:
+            rep[0] = nn.ReLU(inplace=False)
+
+        if strides != 1:
+            rep.append(nn.MaxPool2d(3,strides,1))
+        self.rep = nn.Sequential(*rep)
+
+    def forward(self,inp):
+        x = self.rep(inp)
+
+        if self.skip is not None:
+            skip = self.skip(inp)
+            skip = self.skipbn(skip)
+        else:
+            skip = inp
+
+        x+=skip
+        return x
+
+
+
+class Xception(nn.Module):
+
+    def __init__(self, num_classes=62):
+
+        super(Xception, self).__init__()
+
+        
+        self.num_classes = num_classes
+
+        self.conv1 = nn.Conv2d(1, 32, 3,2, 0, bias=False)
+        self.bn1 = nn.BatchNorm2d(32)
+        self.relu = nn.ReLU(inplace=True)
+
+        self.conv2 = nn.Conv2d(32,64,3,bias=False)
+        self.bn2 = nn.BatchNorm2d(64)
+        #do relu here
+
+        self.block1=Block(64,128,2,2,start_with_relu=False,grow_first=True)
+        self.block2=Block(128,256,2,2,start_with_relu=True,grow_first=True)
+        self.block3=Block(256,728,2,2,start_with_relu=True,grow_first=True)
+
+        self.block4=Block(728,728,3,1,start_with_relu=True,grow_first=True)
+        self.block5=Block(728,728,3,1,start_with_relu=True,grow_first=True)
+        self.block6=Block(728,728,3,1,start_with_relu=True,grow_first=True)
+        self.block7=Block(728,728,3,1,start_with_relu=True,grow_first=True)
+
+        self.block8=Block(728,728,3,1,start_with_relu=True,grow_first=True)
+        self.block9=Block(728,728,3,1,start_with_relu=True,grow_first=True)
+        self.block10=Block(728,728,3,1,start_with_relu=True,grow_first=True)
+        self.block11=Block(728,728,3,1,start_with_relu=True,grow_first=True)
+
+        self.block12=Block(728,1024,2,2,start_with_relu=True,grow_first=False)
+
+        self.conv3 = SeparableConv2d(1024,1536,3,1,1)
+        self.bn3 = nn.BatchNorm2d(1536)
+
+        #do relu here
+        self.conv4 = SeparableConv2d(1536,2048,3,1,1)
+        self.bn4 = nn.BatchNorm2d(2048)
+
+        self.fc = nn.Linear(2048, num_classes)
+
+
+
+        #------- init weights --------
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+        #-----------------------------
+
+
+
+
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.relu(x)
+        
+        x = self.block1(x)
+        x = self.block2(x)
+        x = self.block3(x)
+        x = self.block4(x)
+        x = self.block5(x)
+        x = self.block6(x)
+        x = self.block7(x)
+        x = self.block8(x)
+        x = self.block9(x)
+        x = self.block10(x)
+        x = self.block11(x)
+        x = self.block12(x)
+        
+        x = self.conv3(x)
+        x = self.bn3(x)
+        x = self.relu(x)
+        
+        x = self.conv4(x)
+        x = self.bn4(x)
+        x = self.relu(x)
+
+        x = F.adaptive_avg_pool2d(x, (1, 1))
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+
+        return x
+
+
+if __name__ == "__main__":
+
+
+    model = Xception().cuda()
+    criterion = nn.MSELoss()
+    optimizer = optim.Adam(model.parameters(), lr=0.00005, betas=(0.9, 0.99))
+    optimizer.zero_grad()
+
+
+    data = pd.read_csv("../data/train.csv")
+    data = data.drop(["Feature_1","Feature_16"],axis=1)
+    data = data.fillna(data.mean())
+    data = data.values
+    X = data[:,1:145]
+    Y = data[:,145:-2]
+    X = X.reshape(40000,1,12,12)
+
+
+    lss_lst = []
+
+    for epoch in range(20):
+        for sli in range(30):
+            Xdata = Variable(torch.from_numpy(X[(sli+10)*1000:(sli+11)*1000]).float()).cuda()
+            Ydata = Variable(torch.from_numpy(Y[(sli+10)*1000:(sli+11)*1000]).float()).cuda()
+            optimizer.zero_grad() 
+            out = model(Xdata)
+            loss = criterion(out, Ydata)              
+            loss.backward()                    
+            optimizer.step()
+            if sli % 10 == 0:
+                loss = loss.cpu()
+                print(loss)
+                lss_lst.append(loss.data.numpy()[0])
+
+
+    tst_lss = []
+
+    for sli in range(10):
+        Xdata = Variable(torch.from_numpy(X[sli*1000:(sli+1)*1000]).float()).cuda()
+        Ydata = Variable(torch.from_numpy(Y[sli*1000:(sli+1)*1000]).float()).cuda()
+        out = model(Xdata)
+        loss = criterion(out, Ydata)              
+        if sli % 10 == 0:
+            loss = loss.cpu()
+            print(loss)
+            tst_lss.append(loss.data.numpy()[0])
+
+    print("测试MSE损失：")
+    print(np.array(tst_lss).mean())
+
+
+    plt.figure(figsize=(10,6))
+    plt.plot(list(range((20 * 3) +1))[1:], lss_lst, label='MSE_loss')
+    plt.legend()
+    plt.savefig('MSE_loss.png', dpi=200)
+
+
+    torch.save(model.state_dict(), 'Xception.pkl')  
